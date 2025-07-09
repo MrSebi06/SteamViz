a2f7e90658e42d3273cb78713180ba13e9742683
MrSebi06 <mihneasebastian06@gmail.com>
2025-07-10 00:48:05 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# HOW TO RUN
+
+# 1. Install dependencies: `npm install`
+
+# 2. Start the server: `npx vite`
+
+# 3. Open the browser: `http://localhost:5173/`
---
+++ index.js
@@ -0,0 +1,16 @@
+import ForceGraph3D from "3d-force-graph";
+
+const N = 300;
+const gData = {
+  nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+  links: [...Array(N).keys()]
+    .filter((id) => id)
+    .map((id) => ({
+      source: id,
+      target: Math.round(Math.random() * (id - 1)),
+    })),
+};
+
+const Graph = new ForceGraph3D(document.getElementById("3d-graph")).graphData(
+  gData,
+);
