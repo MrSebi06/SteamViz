fc3368c64e0daea0019d985d0dabfeace5048f63
MrSebi06 <mihneasebastian06@gmail.com>
2025-07-10 00:59:16 +0200

    Add focus on click

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 import ForceGraph3D from "3d-force-graph";
 
-const N = 300;
-const gData = {
-  nodes: [...Array(N).keys()].map((i) => ({ id: i })),
-  links: [...Array(N).keys()]
-    .filter((id) => id)
-    .map((id) => ({
-      source: id,
-      target: Math.round(Math.random() * (id - 1)),
-    })),
-};
+const elem = document.getElementById("3d-graph");
 
-const Graph = new ForceGraph3D(document.getElementById("3d-graph")).graphData(
-  gData,
-);
+const Graph = new ForceGraph3D(elem)
+  .jsonUrl("miserables.json")
+  .nodeLabel("id")
+  .nodeAutoColorBy("group")
+  .onNodeClick((node) => {
+    // Aim at node from outside it
+    const distance = 40;
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
 
-function resizeGraph() {
-  const container = document.getElementById("3d-graph");
-  const width = container.clientWidth;
-  const height = container.clientHeight;
+    const newPos =
+      node.x || node.y || node.z
+        ? {
+            x: node.x * distRatio,
+            y: node.y * distRatio,
+            z: node.z * distRatio,
+          }
+        : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
 
-  Graph.width(width).height(height);
-}
+    Graph.cameraPosition(
+      newPos, // new position
+      node, // lookAt ({ x, y, z })
+      3000, // ms transition duration
+    );
+  });
 
 resizeGraph();
 
