bed87ff402c2285af93ff15e5c2e1bccc98081af
MrSebi06 <mihneasebastian06@gmail.com>
2025-07-11 09:31:14 +0200

    Add data

--- data/main.py
+++ data/main.py
@@ -7,6 +7,20 @@ STEAM_API_KEY = "694E41E2DD3E371E968CCBBB0882B945"
 STEAM_API_URL = "https://api.steampowered.com/"
 
 
+def get_top_games(limit=30):
+    TOP_GAMES_URL = (
+        STEAM_API_URL + "ISteamChartsService/GetGamesByConcurrentPlayers/v1/"
+    )
+    params = {
+        "key": STEAM_API_KEY,
+    }
+    response = requests.get(TOP_GAMES_URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+    game_ids = [game.get("appid") for game in data.get("response", {}).get("ranks", [])]
+    return game_ids[:limit] if limit > 0 else game_ids
+
+
 def get_games_reviews(app_ids):
     reviewers = []
     for app_id in app_ids:
@@ -119,9 +133,8 @@ def get_graph_data():
         )
         params = {"appid": game_id}
         response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            game["players"] = data.get("response", {}).get("player_count", 0)
+        data = response.json()
+        game["players"] = data.get("response", {}).get("player_count", 0)
         link_number = sum(
             1
             for link in graph_data["links"]
@@ -142,6 +155,9 @@ def get_graph_data():
         links_max_value = graph_data["links"][0]["value"] if graph_data["links"] else 0
         node_min_links = min((node["links"] for node in graph_data["nodes"]), default=0)
         node_max_links = max((node["links"] for node in graph_data["nodes"]), default=0)
+        with open("user_ids.json", "r", encoding="utf-8") as user_ids_file:
+            user_ids_data = json.load(user_ids_file)
+            user_ids = user_ids_data.get("user_ids", [])
         json.dump(
             {
                 "node_min_value": node_min_value,
@@ -152,6 +168,7 @@ def get_graph_data():
                 "node_max_links": node_max_links,
                 "nodes_count": len(graph_data["nodes"]),
                 "links_count": len(graph_data["links"]),
+                "player_accounts_analyzed": len(user_ids),
             },
             file,
             indent=4,
@@ -162,6 +179,6 @@ def get_graph_data():
         json.dump(graph_data, file, indent=4, ensure_ascii=False)
 
 
-# get_games_reviews([1903340, 1245620])
+# get_games_reviews(get_top_games())
 # get_users_games()
 get_graph_data()
